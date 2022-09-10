2ba5a03f39328351f12a41c362c7b461d477c48b
unknown <buithuyvan100@gmail.com>
2022-09-10 11:08:04 +0800

    version 1

---
+++ gcd.v
@@ -0,0 +1,105 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/09/06 22:29:31
+// Design Name: 
+// Module Name: gcd
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module gcd(
+    input wire CLK,
+    input wire RST_N,
+    input wire[7:0] A,
+    input wire[7:0] B,
+    input wire START,
+    output reg [7:0] Y,
+    output reg DONE,
+    output reg ERROR
+);
+
+wire found, swap;
+reg [7:0] reg_a, reg_b, data_a, data_b;
+reg [7:0] diff;
+reg error_next;
+reg [1:0] state, state_next;
+
+parameter [1:0] IDLE = 2'b00;
+parameter [1:0] CALC = 2'b01;
+parameter [1:0] FINISH = 2'b10;
+
+// [HW]
+//Finish this design based on
+//the block diagram:
+//   1. FSM
+//   2. Datapath diagram
+//
+ always@(posedge CLK or negedge RST_N)begin
+    if(!RST_N)begin
+        state <= IDLE;
+        ERROR <= 0;
+    end else  begin
+        state <= state_next;
+        ERROR <= error_next;
+    end
+ end
+ 
+ always@(*)begin
+    DONE = 0;
+    Y = 0;
+    case(state)
+    IDLE: begin
+        if(START)begin
+            if(swap)begin
+                reg_a = B;
+                reg_b = A;
+            end else begin
+                reg_a = A;
+                reg_b = B;
+            end
+            state_next = CALC;
+            error_next = reg_a == 0 | reg_b == 0;
+        end else begin
+            state_next = IDLE;
+            error_next = 0;
+        end
+    end
+    CALC: begin
+        error_next = ERROR;
+        if(found || ERROR)begin
+            state_next = FINISH;
+        end else begin
+            state_next = CALC;
+            diff = reg_a%reg_b;
+            reg_a = reg_b;
+            reg_b = diff;    
+        end
+    end
+    FINISH: begin
+        state_next = IDLE;
+        error_next = 0;
+        if(!ERROR)
+           Y = reg_a;
+        DONE = 1;
+    end
+    endcase
+ end
+
+assign swap = (A < B)? 1 : 0;
+assign found = (reg_b == 0 && !ERROR)? 1 : 0;       
+ 
+            
+endmodule
---
+++ gcd_tb.v
@@ -0,0 +1,115 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/09/06 22:35:51
+// Design Name: 
+// Module Name: gcd_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module gcd_tb;
+    parameter cyc = 10;
+    parameter delay = 1;
+    
+    reg clk, rst_n, start;
+    reg [7:0] a, b;
+    wire done, error;
+    wire [7:0] y;
+    
+    gcd gcd01(
+        clk,
+        rst_n,
+        a,
+        b,
+        start,
+        y,
+        done,
+        error
+
+);
+    always #(cyc/2) clk = ~clk; //clock
+    
+    initial begin
+        //$fsdbDumpfile("gcd.fsdb");
+        //$fsdbDumpvars;
+        //$sdf_annotate ("gcd_syn.sdf", gcd01);
+        $dumpfile("gcd.vcd");
+		$dumpvars(0, gcd01);
+        $monitor($time, "CLK=%b RST_N=%b START=%b A=%b B=%b | DONE=%b Y=%b ERROR=%b", 
+        clk, rst_n, start, a, b, done,
+         y, error);
+     end
+     
+     initial begin
+        clk = 1;
+        rst_n = 1;
+        #(cyc);
+        #(delay) rst_n = 0;
+        #(cyc*4) rst_n = 1;
+        #(cyc*2);
+        
+        #(cyc) nop;
+        
+        #(cyc) load; data_in(8'd6, 8'd21);
+        #(cyc) nop;
+        @(posedge done)
+        
+        //#(cyc) nop;
+        #(delay)
+        #(cyc) load; data_in(8'd5, 8'd15);
+        #(cyc) nop;
+        @(posedge done)
+        
+        //#(cyc) nop;
+        #(delay)
+        #(cyc) load; data_in(8'd0, 8'd15);
+        #(cyc) nop;
+        @(posedge done)
+        
+        
+        //#(cyc) nop;
+        #(delay)
+        #(cyc) load; data_in(8'd17, 8'd0);
+        #(cyc) nop;
+        @(posedge done)
+//[HW] apply more patterns to cover 
+// different conditons
+
+        #(cyc) nop;
+        #(cyc*8);
+        $finish;
+    end
+    
+//using tasks to improve the readability
+    task nop;
+        begin
+            start = 0;
+        end
+    endtask
+    task load;
+        begin
+            start = 1;
+        end
+    endtask
+    task data_in;
+        input [7:0] data1, data2;
+        begin
+            a = data1;
+            b = data2;
+        end
+    endtask
+    
+endmodule
